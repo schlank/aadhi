63a92418e5889d43e853550e3838fcbac4f7cb34
Leder, Phil <Phil.Leder@nordstrom.com>
2018-09-04 16:35:46 -0700

    What is the game called.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   include ApplicationHelper
 
   protected
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'USER_ID' && password == 'PASSWORD'
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == USER_ID && password == PASSWORD
+      end
     end
-  end
 end
