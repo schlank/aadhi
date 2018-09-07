3577b97e6cb203a1ce5ee58c1ee54724505617b3
Leder, Phil <Phil.Leder@nordstrom.com>
2018-09-07 15:21:00 -0700

    Should not auth here.

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,5 +1,5 @@
 class ScenariosController < ApplicationController
-  before_filter :authenticate_user
+  # before_filter :authenticate_user
 	skip_before_action :verify_authenticity_token
 
 	def index
