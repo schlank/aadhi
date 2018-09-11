f580731f15fe4a99fec622ca4581ccd0cb613907
Leder, Phil <Phil.Leder@nordstrom.com>
2018-09-10 17:40:14 -0700

    Responding to app client was rejected.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -7,7 +7,7 @@
   class DevicesController < ApplicationController
 
 	use Rack::MethodOverride
-  before_filter :authenticate_user, :except => [:set_scenario]
+  before_filter :authenticate_user, :except => [:set_scenario, :respond_to_app_client]
 	skip_before_filter :verify_authenticity_token
 
     include DevicesHelper
