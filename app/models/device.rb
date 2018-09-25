de970f21a310be277c09fcf02a8d02962a29ae82
Leder, Phil <Phil.Leder@nordstrom.com>
2018-09-24 21:59:59 -0700

    may be this will work.

--- app/models/device.rb
+++ app/models/device.rb
@@ -4,6 +4,7 @@ class Device < ActiveRecord::Base
 
     def find_route(get_path_query, method)
       logger.fatal "find_route.scenario_id: " + scenario_id.to_s
+      logger.fatal "find_route.scenario_scenario_name: " + scenario_scenario_name.to_s
    		Rails.cache.fetch([:scenario, scenario_id, get_path_query, method]) do
     	 	route = scenario.routes.find_by(:path=>get_path_query, :route_type=>method)
      	end
