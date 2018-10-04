31b5a8aa60182df1213e87b1140ea57e2ec5998b
Leder, Phil <Phil.Leder@nordstrom.com>
2018-10-03 17:30:17 -0700

    logger not log.

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -22,8 +22,8 @@ module FeaturesHelper
 					scenarios.update(:scenario_name=>(scenario/'./scenario-name').text)
 	
 			    	(scenario/'routes/route').each {|route|
-              log.fatal ":path=>(route/'./path').text" + :path=>(route/'./path').text
-              log.fatal "route/'./path'" + route/'./path'
+              logger.fatal ":path=>(route/'./path').text" + :path=>(route/'./path').text
+              logger.fatal "route/'./path'" + route/'./path'
 			    		route_path = prune_query_parameters(:path=>(route/'./path').text)
               routes = scenarios.routes.find_or_initialize_by(:path=>route_path, :route_type=>(route/'./route-type').text)
 						routes.update(:route_type=>(route/'./route-type').text,:path=>route_path,:request_body=>(route/'./request-body').text,:fixture=>(route/'./fixture').text,:status=>(route/'./status').text,:host=>(route/'./host').text)
