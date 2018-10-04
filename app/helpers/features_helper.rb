d573f0ee92298bd38fbff42e5de0b5a74061dd59
Leder, Phil <Phil.Leder@nordstrom.com>
2018-10-03 17:23:12 -0700

    This is getting annoying

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -17,13 +17,14 @@ module FeaturesHelper
 			  	flows = feature_model.flows.find_or_initialize_by(:flow_name=>(flow/'./flow-name').text)
 				flows.update(:flow_name=>(flow/'./flow-name').text)
 
-
 			    (flow/'scenarios/scenario').each {|scenario|
 			    	scenarios = flows.scenarios.find_or_initialize_by(:scenario_name=>(scenario/'./scenario-name').text)
 					scenarios.update(:scenario_name=>(scenario/'./scenario-name').text)
 	
 			    	(scenario/'routes/route').each {|route|
-			    		route_path = prune_query_parameters(route/'./path'.text)
+              log.fatal ":path=>(route/'./path').text" + :path=>(route/'./path').text
+              log.fatal "route/'./path'" + route/'./path'
+			    		route_path = prune_query_parameters(:path=>(route/'./path').text)
               routes = scenarios.routes.find_or_initialize_by(:path=>route_path, :route_type=>(route/'./route-type').text)
 						routes.update(:route_type=>(route/'./route-type').text,:path=>route_path,:request_body=>(route/'./request-body').text,:fixture=>(route/'./fixture').text,:status=>(route/'./status').text,:host=>(route/'./host').text)
 
